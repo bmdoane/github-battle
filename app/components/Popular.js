aaad16411541c10c8e7164c6ceeac3e2dd339ceb
Brian Doane <brianmdoane@gmail.com>
2019-05-22 10:30:56 -0500

    Added button updating language selection

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -8,14 +8,24 @@ export default class Popular extends React.Component {
       selectedLanguage: 'All'
     }
   }
+  updateLanguage (selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    })
+
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
-
+    // console.log(this);
     return (
       <ul className='flex-center'>
         {languages.map((language) => (
           <li key={language}>
-            <button className='btn-clear nav-link'>
+            <button
+              className='btn-clear nav-link'
+              style={language === this.state.selectedLanguage ? { color: '#E7000C'} : null}
+              onClick={() => this.updateLanguage(language)}>
               {language}
             </button>
           </li>
