36809e7e0e9d5b67e1e70286f6e3b9d0246d944d
Brian Doane <brianmdoane@gmail.com>
2019-05-29 00:06:21 -0500

    Added default props to Loading component, component is uploadable and custamizable

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const styles = {
   content: {
@@ -16,15 +17,17 @@ export default class Loading extends React.Component {
     super(props)
 
     this.state = {
-      content: 'Loading'
+      content: props.text
     }
   }
   componentDidMount () {
+    const { speed, text } = this.props
+
     this.interval = window.setInterval(() => {
-      this.state.content === 'Loading' + '...'
-        ? this.setState({ content: 'Loading' })
+      this.state.content === text + '...'
+        ? this.setState({ content: text })
         : this.setState(({ content }) => ({ content: content + '.' }))
-    }, 300)
+    }, speed)
   }
   componentWillUnmount () {
     window.clearInterval(this.interval)
@@ -36,5 +39,15 @@ export default class Loading extends React.Component {
       </p>
     )
   }
+}
+
+Loading.propTypes = {
+  text: PropTypes.string.isRequired,
+  speed: PropTypes.number.isRequired
+}
+
+Loading.defaultProps = {
+  text: 'Loading',
+  speed: 300
 }
 
