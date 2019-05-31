df7a39d518c63c395473aad591aff5a55fc747c0
Brian Doane <brianmdoane@gmail.com>
2019-05-31 08:24:36 -0500

    Created Tooltip component, refactored Results ProfileList from class based back to functional. Intent is to use tooltip on multiple components

---
+++ app/components/Tooltip.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const styles = {
+  container: {
+    position: 'relative',
+    display: 'flex'
+  },
+  tooltip: {
+    boxSizing: 'border-box',
+    position: 'absolute',
+    width: '160px',
+    bottom: '100%',
+    left: '50%',
+    marginLeft: '-80px',
+    borderRadius: '3px',
+    backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
+    padding: '7px',
+    marginBottom: '5px',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: '14px'
+  }
+}
+
+export defaults class Tooltip extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hovering: false,
+    }
+
+    this.mouseOver = this.mouseOver.bind(this)
+    this.mouseOut = this.mouseOut.bind(this)
+  }
+
+  mouseOver() {
+    this.setState({
+      hovering: true
+    })
+  }
+
+  mouseOut() {
+    this.setState({
+      hovering: false
+    })
+  }
+
+  render() {
+    return (
+      <div
+        onMouseOver={this.mouseOver}
+        onMouseOut={this.mouseOut}
+        style={styles.container}
+      >
+        { hovering === true && <div style={styles.tooltip}></div>}
+
+      </div>
+    )
+  }
+}
