894b1e6ce3e5d0649e77fecee1a6340f291f6573
Brian Doane <brianmdoane@gmail.com>
2019-05-31 15:10:27 -0500

    Refactored replacing HOC with render prop pattern for cleaner control of naming collision.

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import withHover from './withHover'
+import Hover from './Hover'
 
 const styles = {
   container: {
@@ -24,20 +24,20 @@ const styles = {
   }
 }
 
-function Tooltip ({ text, children, hovering }) {
+export default function Tooltip ({ text, children }) {
+  // Render prop pattern - receiving the hovering prop as an argument, controlling the naming
   return (
-    <div style={styles.container}>
-      {hovering === true && <div style={styles.tooltip}>{text}</div>}
-      {children}
-    </div>
+    <Hover>
+      {(hovering) => (
+        <div style={styles.container}>
+          {hovering === true && <div style={styles.tooltip}>{text}</div>}
+          {children}
+        </div>
+      )}
+    </Hover>
   )
 }
 
 Tooltip.propTypes = {
-  text: PropTypes.string.isRequired,
-  hovering: PropTypes.bool.isRequired
+  text: PropTypes.string.isRequired
 }
-
-// Exporting the invocation of withHover(Higher Order Component) with Tooltip(Functional Component)
-// 2nd argument string can be used for naming prop, if not using default prop in HOC
-export default withHover(Tooltip)
